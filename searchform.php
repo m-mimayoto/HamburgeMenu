834a748b490dae7a334452a309ba0fc1f3ee9cbf
m-miyamoto <pr.h110816@gmail.com>
2021-07-31 22:37:38 +0900

    searchform作成

--- searchform.php
+++ searchform.php
@@ -1,4 +1,5 @@
 <form class="p-header__search"  action="<?php echo home_url('/'); ?>" method="GET">
-                <input class="fas fa-lg p-header__search__box"  value="&#xf002;" type="search" name="s" id="s">
+                <i class="c-search__icon fas fa-search"></i>
+               <input class="p-header__search__box" type="text" value="<?php the_search_query();?>"  name="s" id="s">
                 <input class="c-button__search" type="submit" value="検索">
 </form>
