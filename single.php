6a47fd47131d7d04f96c815d6523e0a5ef414f77
m-miyamoto <pr.h110816@gmail.com>
2021-07-31 22:34:40 +0900

    single作成

--- single.php
+++ single.php
@@ -1,131 +1,16 @@
-
-    <?php get_header(); ?>
+<?php get_header(); ?>
 
         <div class="wrapper">
-            <main>
-                <section class="p-top">
-                <div class="l-top--single"></div><!--写真-->
-                    <h1 class="p-top__title--single">h1 チーズバーガー</h1>
+        <main>
+        <?php if(have_posts() ) : ?>
+            <?php while(have_posts() ) : the_post(); ?>
+                <section id="post-<?php the_ID(); ?>" <?php post_class('p-top'); ?>>
+                <div class="l-top--single"><?php the_post_thumbnail(); ?></div><!--写真-->
+                    <h1 class="p-top__title--single"><?php the_title(); ?></h1>
                 </section>
-
-                <div class="l-wrapper--single">
-                <section class="p-contents">
-                    <h2>見出しh2</h2>
-                    <p>Pテグテキスト。Pテグテキスト。Pテグテキスト。Pテグテキスト。Pテグテキスト。Pテグテキスト。Pテグテキスト。Pテグテキスト。Pテグテキスト。Pテグテキスト。Pテグテキスト。Pテグテキスト。Pテグテキスト。Pテグテキスト。Pテグテキスト。Pテグテキスト。Pテグテキスト。Pテグテキスト。Pテグテキスト。Pテグテキスト。</p>
-                    <h3>見出しh3</h3>
-                    <h4>見出しh4</h4>
-                    <h4>見出しh3</h4>
-                    <h4>見出しh4</h4>
-                    <h4>見出しh5</h4>
-                    <h4>見出しh6</h4>
-                </section>
-
-                <section class="p-blockquote">
-                    <blockquote class="p-blockquote__text">Blockquote 引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ</blockquote>
-                    <p class="p-blockquote__note">出典元：<span>〇〇〇〇〇〇〇〇〇〇〇〇〇〇</span></p>
-                </section>
-
-                <section class="p-product l-product">
-                    <div class="l-product__img--main"></div><!--写真-->
-                    <figure class="p-product__item--top">
-                        <div class="l-product__item__img--top"></div>
-                        <figcaption class="p-product__item__text--top">テキストが入ります　テキストが入ります　テキストが入ります　テキストが入ります　テキストが入ります　テキストが入ります テキストが入ります　テキストが入ります テキストが入ります　テキストが入ります</figcaption>
-                    </figure>
-                    <figure class="p-product__item--bottom">
-                        <div class="l-product__item__img--bottom"></div>
-                        <figcaption class="p-product__item__text--bottom">テキストが入ります　テキストが入ります テキストが入ります　テキストが入ります テキストが入ります　テキストが入ります　テキストが入ります　テキストが入ります　テキストが入ります　テキストが入ります</figcaption>
-                    </figure>
-                    <div class="l-product__img--sub"></div>
-                </section>
-
-                <section class="p-gallery">
-                    <ul class="p-gallery__column">
-                        <li class="p-gallery__item"><div class="p-gallery__item__img"></div></li>
-                        <li class="p-gallery__item"><div class="p-gallery__item__img"></div></li>
-                        <li class="p-gallery__item"><div class="p-gallery__item__img"></div></li>
-                        <li class="p-gallery__item"><div class="p-gallery__item__img"></div></li>
-                        <li class="p-gallery__item"><div class="p-gallery__item__img"></div></li>
-                        <li class="p-gallery__item"><div class="p-gallery__item__img"></div></li>
-                        <li class="p-gallery__item"><div class="p-gallery__item__img"></div></li>
-                        <li class="p-gallery__item"><div class="p-gallery__item__img"></div></li>
-                        <li class="p-gallery__item"><div class="p-gallery__item__img"></div></li>
-                    </ul>
-                </section>
-
-                <section class="p-secondary1">
-                    <div class="p-secondary1--top">
-                    <ul class="p-secondary1__list">
-                        <li class="p-secondary1__list__item">1.リストリストリスト</li>
-                        <li class="p-secondary1__list__item">2.リストリストリスト</li>
-                    </ul>
-                    <ul class="p-secondary1__list2">
-                        <li class="p-secondary1__list__item">1.リスト2リスト2リスト2</li>
-                        <li class="p-secondary1__list__item">2.リスト2リスト2リスト2</li>
-                    </ul>
-                    <ul class="p-secondary1__list">
-                        <li class="p-secondary1__list__item">1.リストリストリスト</li>
-                        <li class="p-secondary1__list__item">2.リストリストリスト</li>
-                    </ul>
-                    </div>
-                    <div class="p-secondary1--bottom">
-                    <ul class="p-secondary1__list">
-                        <li class="p-secondary1__list__item">リストリストリスト</li>
-                        <li class="p-secondary1__list__item">リストリストリスト</li>
-                    </ul>
-                    <ul class="p-secondary1__list2">
-                        <li class="p-secondary1__list__item">リスト2リスト2リスト2</li>
-                        <li class="p-secondary1__list__item">リスト2リスト2リスト2</li>
-                    </ul>
-                    <ul class="p-secondary1__list">
-                        <li class="p-secondary1__list__item">リストリストリスト</li>
-                        <li class="p-secondary1__list__item">リストリストリスト</li>
-                    </ul>
-                    </div>
-                </section>
-
-                <section class="p-secondary2">
-                    <pre>
-                        <code>
-        &lt;html&gt;
-            &lt;head&gt;
-            &lt;/head&gt;
-            &lt;body&gt;
-            &lt;/body&gt;
-        &lt;/html&gt;
-                        </code>
-                    </pre>
-                </section>
-                
-                <section class="p-secondary3">
-                    <table class="p-secondary3__table" border="1" style="border-collapse: collapse" bgcolor=#fff bordercolor=#707070>
-                        <tr>
-                            <td class="p-seconday3__table__td1">テーブル</td>
-                            <td class="p-seconday3__table__td2">テーブル</td>
-                        </tr>
-                        <tr>
-                            <td class="p-seconday3__table__td1">テーブル</td>
-                            <td class="p-seconday3__table__td2">テーブル</td>
-                        </tr>
-                        <tr>
-                            <td class="p-seconday3__table__td1">テーブル</td>
-                            <td class="p-seconday3__table__td2">テーブル</td>
-                        </tr>
-                        <tr>
-                            <td class="p-seconday3__table__td1">テーブル</td>
-                            <td class="p-seconday3__table__td2">テーブル</td>
-                        </tr>
-                    </table>
-                </section>
-
-                <section class="p-secondary4">
-                    <button class="p-secondary4__button" type="submit">ボタン</button>
-                </section>
-
-                <section class="p-secondary5">
-                    <p class="p-secondary5__text">boldboldboldboldboldboldbold</p>
-                </section>
-
-                </div><!--l-wrapper--single-->
+                <?php the_content(); ?>
+            <?php endwhile; ?>
+        <?php endif; ?>
             </main>
 
             <?php get_sidebar(); ?>
