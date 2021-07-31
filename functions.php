9f9ddb7e7d833f10fa32f6f0b948bab287d742ad
m-miyamoto <pr.h110816@gmail.com>
2021-07-31 22:41:51 +0900

    functions編集

--- functions.php
+++ functions.php
@@ -1,6 +1,30 @@
 <?php
+    function custom_theme_support(){
+        add_theme_support('html5',array(
+            'search-form',
+            'comment-form',
+            'comment-list',
+            'gallery',
+            'caption',
+        ));
+    add_theme_support('post-thumbnails');
     add_theme_support( 'menus' );
     add_theme_support( 'title-tag' );
+    register_nav_menus( array(
+            'side_nav' => esc_html__('side navigation','hamburger'),
+            'footer_nav' => esc_html__('footer navigation','hamburger'),
+        ));
+    }
+    add_action('after_setup_theme','custom_theme_support');
+
+    function hamburger_script() {
+        wp_enqueue_style( 'font-awesome', '//use.fontawesome.com/releases/v5.6.1/css/all.css', array(), 'v5.6.1' );
+        wp_enqueue_style( 'hamburger', get_template_directory_uri() . '/css/hamburger.css', array(), '1.0.0' );
+        wp_enqueue_style( 'style', get_template_directory_uri() . '/style.css', array(), '1.0.0' );
+        wp_enqueue_script( 'script', get_template_directory_uri() . '/move.js', array(), '1.0.0', true );
+    }
+    add_action( 'wp_enqueue_scripts', 'hamburger_script' );
+
 
     function hamburger_title( $title ) {
         if ( is_front_page() && is_home() ) { //表示されたページがフロントページかつメインページなら
@@ -11,26 +35,3 @@
             return $title;
         }
     add_filter( 'pre_get_document_title', 'hamburger_title' ); //
-
-    function hamburger_script() {
-        wp_enqueue_style( 'font-awesome', '//use.fontawesome.com/releases/v5.6.1/css/all.css', array(), 'v5.6.1' );
-        wp_enqueue_style( 'hamburger', get_template_directory_uri() . '/scss/hamburger.css', array(), '1.0.0' );
-        wp_enqueue_style( 'style', get_template_directory_uri() . '/style.css', array(), '1.0.0' );
-        wp_enqueue_script( 'script', get_template_directory_uri() . '/move.js', array(), '1.0.0', true );
-    }
-    add_action( 'wp_enqueue_scripts', 'hamburger_script' );
-
-    function hamburger_widgets_init() {
-        register_sidebar (
-            array(
-                'name'          => 'All Menu',
-                'id'            => 'menu_widget',
-                'description'   => 'メニューの一覧です',
-                'before_widget' => '<div id="%1$s" class="widget %2$s">',
-                'after_widget'  => '</div>',
-                'before_title'  => '<h2 class="c-category-title" style="display:none;">',
-                'after_title'   => "</h2>\n",
-            )
-        );
-    }
-    add_action( 'widgets_init', 'hamburger_widgets_init' );
