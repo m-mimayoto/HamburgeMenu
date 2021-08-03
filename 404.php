0d3a4b810666faf8ccec6942ff839d5fafaab15a
m-miyamoto <pr.h110816@gmail.com>
2021-08-03 15:18:28 +0900

    404修正

--- 404.php
+++ 404.php
@@ -1,6 +1,8 @@
 <?php get_header(); ?>
-   <div id="content" class="narrowcolumn">
-     <h2 class="center">Error 404 - Not Found</h2>
-	</div>
+<div id="main">
+  <h2>404 Not Found（ページが見つかりませんでした）</h2>
+  <p>指定された以下のページは存在しないか、または移動した可能性があります。</p>
+  <p class="error_url">URL ：<span><?php echo get_pagenum_link(); ?></span></p>
+</div>
 <?php get_sidebar(); ?>
 <?php get_footer(); ?>
