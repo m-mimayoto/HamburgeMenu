f6220162042170e49a48e1700fef639d7b1b9881
m-miyamoto <pr.h110816@gmail.com>
2021-08-05 11:16:45 +0900

    404編集

--- 404.php
+++ 404.php
@@ -1,5 +1,5 @@
 <?php get_header(); ?>
-<div id="main">
+<div class="main--error">
   <h2>404 Not Found（ページが見つかりませんでした）</h2>
   <p>指定された以下のページは存在しないか、または移動した可能性があります。</p>
   <p class="error_url">URL ：<span><?php echo get_pagenum_link(); ?></span></p>
