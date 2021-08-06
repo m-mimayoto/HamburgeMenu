87123f2aaf5612e162183cf58075c2dd3d128165
m-miyamoto <pr.h110816@gmail.com>
2021-08-06 11:58:51 +0900

    themecheck

--- footer.php
+++ footer.php
@@ -1,17 +1,13 @@
 <footer class="l-footer">
             <div class="p-footer__contents">
             <?php 
-            $args = array(
-                'theme_location' => 'footer_nav',
-                'container' => 'nav',
-                'container_class' => 'nav',
-                'menu_class' => 'nav_item',
-                //'link_after' => '|'
-            );
-            wp_nav_menu($args);?>
-            
-            <!--wp_nav_menu( array( 'theme_location' => 'footer_nav' ) ); ?>-->
-               <!-- <p class="p-footer__contents__items"><a class="p-footer__contents__item">ショップ情報</a>｜<a class="p-footer__contents__item">ヒストリー</a></p>-->
+                wp_nav_menu(array(
+                    'theme_location' => 'footer_nav',
+                    'container' => 'nav',
+                    'container_class' => 'nav',
+                    'menu_class' => 'nav_item',
+                 ));
+            ?>
                 <p class="p-footer__contents__note">Copyright: RaiseTech</p>
             </div>
 </footer>
