8c1a5c101e3b1a08b763afb34f96a64d602aa201
m-miyamoto <pr.h110816@gmail.com>
2021-08-12 11:24:50 +0900

    themecheck

--- sidebar.php
+++ sidebar.php
@@ -1,19 +1,19 @@
 <aside class="p-aside">
-            <nav class="l-nav">
-                <div class="c-button--close">
-                    <span class="c-button--line"></span>
-                </div>
-                <p class="p-nav__memu">Menu</p>
+    <nav class="l-nav">
+        <div class="c-button--close">
+            <span class="c-button--line"></span>
+        </div>
+        <p class="p-nav__memu">Menu</p>
                
-                <?php 
-                wp_nav_menu(array(
-                    'theme_location' => 'side_nav',
-                    'container' => 'div',
-                    'container_class' => 'nav__contents__list',
-                    'menu_class' => 'nav__contents__list__item',
-                ));
-                ?>
+        <?php 
+            wp_nav_menu(array(
+                'theme_location' => 'side_nav',
+                'container' => 'div',
+                'container_class' => 'nav__contents__list',
+                'menu_class' => 'nav__contents__list__item',
+            ));
+        ?>
 
-            </nav>
-        </aside>
-            <div class="l-aside__layer"></div>
+    </nav>
+</aside>
+<div class="l-aside__layer"></div>
--- single.php
+++ single.php
@@ -1,22 +1,17 @@
 <?php get_header(); ?>
-
-        <div class="wrapper">
+    <div class="wrapper">
         <main>
-        <?php if(have_posts() ) : ?>
+            <?php if(have_posts() ) : ?>
             <?php while(have_posts() ) : the_post(); ?>
-                <section id="post-<?php the_ID(); ?>" <?php post_class('p-top'); ?>>
+            <section id="post-<?php the_ID(); ?>" <?php post_class('p-top'); ?>>
                 <div class="l-top--single"><?php the_post_thumbnail(); ?></div><!--写真-->
-                    <h1 class="p-top__title--single"><?php the_title(); ?></h1>
-                </section>
-                <?php the_content(); ?>
-                <div class="page-links">
-                <?php wp_link_pages(); // 記事内ページャー ?>
-                </div>
+                <h1 class="p-top__title--single"><?php the_title(); ?></h1>
+            </section>
+            <?php the_content(); ?>
+                <div class="page-links"><?php wp_link_pages(); // 記事内ページャー ?></div>
             <?php endwhile; ?>
-        <?php endif; ?>
-            </main>
-
-            <?php get_sidebar(); ?>
-        </div><!--wrapper-->
-
-        <?php get_footer(); ?> 
+            <?php endif; ?>
+        </main>
+        <?php get_sidebar(); ?>
+    </div><!--wrapper-->
+<?php get_footer(); ?> 
