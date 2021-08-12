73f88f0631477c10762ac91b73935563b95c822a
m-miyamoto <pr.h110816@gmail.com>
2021-08-12 11:24:20 +0900

    themecheck

--- page.php
+++ page.php
@@ -1,18 +1,16 @@
 <?php get_header(); ?>
-        <div class="wrapper">
-            <main>
+    <div class="wrapper">
+        <main>
             <?php if(have_posts() ) : ?>
-                <?php while(have_posts() ) : the_post(); ?>
-                <section id="post-<?php the_ID(); ?>" <?php post_class('p-top'); ?>>
+            <?php while(have_posts() ) : the_post(); ?>
+            <section id="post-<?php the_ID(); ?>" <?php post_class('p-top'); ?>>
                 <div class="l-top--page"><?php the_post_thumbnail(); ?></div><!--写真-->
-                    <h1 class="p-top__title--page"><?php the_title(); ?></h1>
-                </section>
-                <?php the_content(); ?>
-                <?php endwhile; ?>
+                <h1 class="p-top__title--page"><?php the_title(); ?></h1>
+            </section>
+            <?php the_content(); ?>
+            <?php endwhile; ?>
             <?php endif; ?>
-            </main>
-
-            <?php get_sidebar(); ?>
-        </div><!--wrapper-->
-
-        <?php get_footer(); ?>
+        </main>
+        <?php get_sidebar(); ?>
+    </div><!--wrapper-->
+<?php get_footer(); ?>
--- search.php
+++ search.php
@@ -1,38 +1,33 @@
-    <?php get_header(); ?>
-
-        <div class="wrapper">
-            <main>
-                <section class="p-top">
+<?php get_header(); ?>
+    <div class="wrapper">
+        <main>
+            <section class="p-top">
                 <div class="l-top__title__layer"></div>
                 <div class="l-top--archive"></div>
-                    <h2 class="p-top__title--search">Search:</h2>
-                    <p class="p-top__subtitle--archive_search"><?php echo esc_html( get_search_query( false ) ); ?></p>
-                </section>
+                <h2 class="p-top__title--search">Search:</h2>
+                <p class="p-top__subtitle--archive_search"><?php echo esc_html( get_search_query( false ) ); ?></p>
+            </section>
 
-                <section class="p-menu">
-                    <article class="p-menu__desc">
-                        <h2 class="p-menu__desc__title"><?php echo esc_html( get_search_query( false ) ); ?></h2>
-                        <p class="p-menu__desc__text">テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。</p>
-                    
-                    </article>
+            <section class="p-menu">
+                <article class="p-menu__desc">
+                    <h2 class="p-menu__desc__title"><?php echo esc_html( get_search_query( false ) ); ?></h2>
+                    <p class="p-menu__desc__text">テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。</p>
+                </article>
 
-                    <?php if(have_posts()) : ?>
-                        <?php while(have_posts()) : the_post(); ?>
-                        <?php get_template_part('sample'); ?>
-                            <?php 
-                            endwhile;
-                        else:?><p class="errormessage">表示できる記事がありません。</p>
-                     <?php endif; ?>
+                <?php if(have_posts()) : ?>
+                    <?php while(have_posts()) : the_post(); ?>
+                    <?php get_template_part('sample'); ?>
+                    <?php 
+                        endwhile;
+                    else:?><p class="errormessage">表示できる記事がありません。</p>
+                    <?php endif; ?>
                     
                     <section class="p-menu__page">
-
                     </section><!--p-menu__page-->
-                </section><!--p-menu-->
-            </main>
-
-            <?php wp_pagenavi(); ?>
-            <?php get_sidebar(); ?>
-        </div><!--wrapper-->
-
-        <?php get_footer(); ?> 
+            </section><!--p-menu-->
+        </main>
+    <?php wp_pagenavi(); ?>
+    <?php get_sidebar(); ?>
+    </div><!--wrapper-->
+<?php get_footer(); ?> 
     
